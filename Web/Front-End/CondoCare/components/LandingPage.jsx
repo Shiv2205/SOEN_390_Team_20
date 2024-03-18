e95cdf528311cd1d42930aee3a477111a55e3296
Aly Hussein <alymohmadeduc@hotmail.co.uk>
2024-03-18 17:53:02 -0400

    implemented landing page with bootstrap styling

---
+++ Web/Front-End/CondoCare/components/LandingPage.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import { Navigation } from "./LandingPageComponents/navigation";
+import { Header } from "./LandingPageComponents/header";
+import { Features } from "./LandingPageComponents/features";
+import { About } from "./LandingPageComponents/about";
+import { Services } from "./LandingPageComponents/services";
+import { Gallery } from "./LandingPageComponents/gallery";
+import { Testimonials } from "./LandingPageComponents/testimonials";
+import { Team } from "./LandingPageComponents/Team";
+import { Contact } from "./LandingPageComponents/contact";
+import JsonData from "./LandingPageComponents/data.json";
+
+function LandingPage() {
+    const [landingPageData, setLandingPageData] = useState({});
+  useEffect(() => {
+    setLandingPageData(JsonData);
+  }, []);
+    return (
+        <div>
+          <Navigation />
+          <Header data={landingPageData.Header} />
+          <Features data={landingPageData.Features} />
+          <About data={landingPageData.About} />
+          <Services data={landingPageData.Services} />
+          <Gallery data={landingPageData.Gallery} />
+          <Testimonials data={landingPageData.Testimonials} />
+          <Team data={landingPageData.Team} />
+          <Contact data={landingPageData.Contact} />
+        </div>
+      );
+};
+
+export default LandingPage;
--- Web/Front-End/CondoCare/src/main.jsx
+++ Web/Front-End/CondoCare/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// import ReactDOM from 'react-dom';
 import App from './App.jsx'
 import './index.css'
 
@@ -8,3 +9,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
