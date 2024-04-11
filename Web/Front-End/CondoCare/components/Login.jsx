83e35fc01d823ba801db02b22cf5bd015505c8f0
Shivam Veerabudren <92755248+Shiv2205@users.noreply.github.com>
2024-04-10 21:01:51 -0400

    added google auth button (front-end)

--- Web/Front-End/CondoCare/components/Login.jsx
+++ Web/Front-End/CondoCare/components/Login.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { Navigation } from "./LandingPageComponents/navigation";
 import { useStore } from "../store/store";
 
-function Login({setUserData }) {
+function Login({ setUserData }) {
   const [errMessage, setErrMessage] = useState("");
   const navigate = useNavigate(); // Hook for navigation
   const dispatch = useStore()[1];
@@ -25,16 +25,16 @@ function Login({setUserData }) {
       fullName: "John Doe",
       age: 30,
       phone: 5146010320,
-      address: "232 jjjd street"
+      address: "232 jjjd street",
     };
-    
+
     // Store user data in localStorage
     localStorage.setItem("userData", JSON.stringify(boilerplateUserData));
-    
+
     // Set the boilerplate user data using setUserData
     setUserData(boilerplateUserData);
 
-    if (!userData)formErrors.push("Email or password is incorrect");
+    if (!userData) formErrors.push("Email or password is incorrect");
 
     if (formErrors.length > 0) {
       let tempError = "Please fix the following to continue:\n";
@@ -42,17 +42,15 @@ function Login({setUserData }) {
         tempError += `  ${index + 1}. ${err}\n`;
       });
       setErrMessage(tempError);
-    } 
-    else {
+    } else {
       navigate("/userDashboard");
     }
   };
 
   return (
-
-    <div style={{ overflow: 'hidden' }}>
+    <div style={{ overflow: "hidden" }}>
       <Navigation />
-      <div className="login-container" style={{ marginTop: '110px'}}>
+      <div className="login-container" style={{ marginTop: "110px" }}>
         <h2>Login</h2>
 
         <div className="error-message">{errMessage ? errMessage : ""}</div>
@@ -66,9 +64,18 @@ function Login({setUserData }) {
 
           <button type="submit">Login</button>
         </form>
+        <div>
+        <GoogleAuth/>
+        </div>
         <p>
           Don't have an account?{" "}
-          <a href="" onClick={(e) => {e. preventDefault(); navigate("/signup")}}>
+          <a
+            href=""
+            onClick={(e) => {
+              e.preventDefault();
+              navigate("/signup");
+            }}
+          >
             Sign up
           </a>
         </p>
@@ -107,14 +114,47 @@ function getUserData(formData, dispatch) {
     .then((data) => {
       // Handle the response from the server
       // Add user data to App-wide state
-      dispatch("CREATE", {userData: {...data.loginData}});
-      if (data.response === "Email or password is incorrect") throw new Error(data.response);
+      dispatch("CREATE", { userData: { ...data.loginData } });
+      if (data.response === "Email or password is incorrect")
+        throw new Error(data.response);
     })
     .catch((error) => {
       // Handle error
       console.error("Error:", error);
     });
-    return userData;
+  return userData;
 }
 
 export default Login;
+
+const GoogleAuth = () => {
+  return (
+    <div className="google-button btn">
+      <div style={{ display: "flex", justifyContent: "flex-start" }}>
+        <span className="svgIcon t-popup-svg">
+          <svg className="svgIcon-use" style={{float: 'left'}} width="30" height="40" viewBox="0 0 25 25">
+            <g fill="none" fill-rule="evenodd">
+              <path
+                d="M20.66 12.693c0-.603-.054-1.182-.155-1.738H12.5v3.287h4.575a3.91 3.91 0 0 1-1.697 2.566v2.133h2.747c1.608-1.48 2.535-3.65 2.535-6.24z"
+                fill="#4285F4"
+              />
+              <path
+                d="M12.5 21c2.295 0 4.22-.76 5.625-2.06l-2.747-2.132c-.76.51-1.734.81-2.878.81-2.214 0-4.088-1.494-4.756-3.503h-2.84v2.202A8.498 8.498 0 0 0 12.5 21z"
+                fill="#34A853"
+              />
+              <path
+                d="M7.744 14.115c-.17-.51-.267-1.055-.267-1.615s.097-1.105.267-1.615V8.683h-2.84A8.488 8.488 0 0 0 4 12.5c0 1.372.328 2.67.904 3.817l2.84-2.202z"
+                fill="#FBBC05"
+              />
+              <path
+                d="M12.5 7.38c1.248 0 2.368.43 3.25 1.272l2.437-2.438C16.715 4.842 14.79 4 12.5 4a8.497 8.497 0 0 0-7.596 4.683l2.84 2.202c.668-2.01 2.542-3.504 4.756-3.504z"
+                fill="#EA4335"
+              />
+            </g>
+          </svg>
+        </span>
+        <span className="button-label">Sign in with Google</span>
+      </div>
+    </div>
+  );
+};
